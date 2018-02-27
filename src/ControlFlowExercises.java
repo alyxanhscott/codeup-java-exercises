a2e30510f9e62e54edc12c48c25edd329f67fe4c
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-27 16:58:56 -0600

    table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,24 +2,18 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to go up to: ");
+        int userNum = sc.nextInt();
 
-        for (int i = 0; i <= 100 ; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else {
-                System.out.println(i);
-            }
+        String tableFormat = "%-6d | %-7d | %-5d%n";
+        System.out.format("number | squared | cubed%n");
 
+        for (int i = 1; i <= userNum ; i++) {
+            int squ = (int)(Math.pow(i, 2));
+            int cub = (int)(Math.pow(i, 3));
+            System.out.format(tableFormat, i, squ, cub);
         }
-
-
     }
-
 }
 
