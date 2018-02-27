00cd0b9cfb3049f5b5c6e32b5c6ea0fc4c89a6f7
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-27 15:29:35 -0600

    do while 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,10 +4,10 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i = 100;
+        int i = 2;
         do {
         System.out.println("i is: " + i);
-            i -= 5;
-        } while (i >= -10);
+            i *= 2;
+        } while (i <= 1000000);
     }
 }
