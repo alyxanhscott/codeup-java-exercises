83c069fc31691ca73f0cb2cf223488861a454ec6
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-27 12:03:06 -0600

    update java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int myFavNum = 144;
+        String myString = "The eye of the tiger";
+        float myNumber = 3.14f;
+        int x = 4;
+        int y = 4;
+
+//        System.out.println(x += 5);
+//        System.out.println(y *= x);
+//        System.out.println(y /= x);
+//        System.out.println(y -= x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
         System.out.println("Hello, dude!");
         System.out.print("Hello, World!");
+        System.out.println(myFavNum);
+        System.out.println(myString);
+        System.out.println(myNumber);
+
+
 
     }
 }
