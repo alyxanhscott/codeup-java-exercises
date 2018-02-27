a2bd22f373ed06250fd27cb6798c67e8ee33d68a
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-27 15:14:51 -0600

    git controlflow while

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 5;
+        while (i <= 15) {
+            System.out.println("i is: " + i);
+            i++;
+        }
+    }
+}
