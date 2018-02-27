0b6f2f4c083cfcc4f739686682d3b9ebcf74c4c7
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-27 15:36:23 -0600

    for loop 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,7 +4,7 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 100; i >= -10; i-= 5) {
+        for (int i = 2; i <= 1000000; i*= 2) {
             System.out.println("i is: " + i);
         }
     }
