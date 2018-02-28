8b0dfcf1a0e55994ebabb8acab2c31fa5df3063a
alyxandriascott <alyxan.hope26@gmail.com>
2018-02-28 09:49:29 -0600

    table complete java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to go up to: ");
-        int userNum = sc.nextInt();
 
-        String tableFormat = "%-6d | %-7d | %-5d%n";
-        System.out.format("number | squared | cubed%n");
+        do {
+            System.out.println("Enter a number to go up to: ");
+            int userNum = sc.nextInt();
 
-        for (int i = 1; i <= userNum ; i++) {
-            int squ = (int)(Math.pow(i, 2));
-            int cub = (int)(Math.pow(i, 3));
-            System.out.format(tableFormat, i, squ, cub);
-        }
+            String tableFormat = "%-6d | %-7d | %-5d%n";
+            System.out.format("number | squared | cubed%n");
+
+            for (int i = 1; i <= userNum ; i++) {
+                int squ = (int)(Math.pow(i, 2));
+                int cub = (int)(Math.pow(i, 3));
+                System.out.format(tableFormat, i, squ, cub);
+            }
+            System.out.println("Would you like to continue? Type yes or no: ");
+        } while ("yes".equalsIgnoreCase(sc.next()));
+
     }
 }
 
